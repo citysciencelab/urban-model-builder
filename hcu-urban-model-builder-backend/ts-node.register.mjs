dd8a5e91338edf5e44794a4ebc2b88a56c217735
Nico Lazarus <n.lazarus@triggerco.de>
2024-08-28 16:01:57 +0200

    🐞fixed ts error handling for 'npm run dev'

--- hcu-urban-model-builder-backend/ts-node.register.mjs
+++ hcu-urban-model-builder-backend/ts-node.register.mjs
@@ -1,4 +1,10 @@
 import { pathToFileURL } from "node:url";
 import { register } from "node:module";
+import { setUncaughtExceptionCaptureCallback } from "node:process";
+
+setUncaughtExceptionCaptureCallback((error) => {
+  console.error(error);
+  process.exit(1);
+});
 
 register("ts-node/esm", pathToFileURL("./"));
