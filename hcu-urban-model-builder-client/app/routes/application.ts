62f8c4d2778ac3dea81084a193c920b43ad5e8a4
Nico Lazarus <n.lazarus@triggerco.de>
2024-09-02 16:46:05 +0200

    ✨ redirect to "models" route

--- hcu-urban-model-builder-client/app/routes/application.ts
+++ hcu-urban-model-builder-client/app/routes/application.ts
@@ -1,3 +1,14 @@
 import Route from '@ember/routing/route';
+import type RouterService from '@ember/routing/router-service';
+import type Transition from '@ember/routing/transition';
+import { service } from '@ember/service';
 
-export default class ApplicationRoute extends Route { }
+export default class ApplicationRoute extends Route {
+  @service declare router: RouterService;
+
+  beforeModel(transition: Transition) {
+    if (transition.to?.name === 'authenticated.index') {
+      this.router.replaceWith('models');
+    }
+  }
+}
