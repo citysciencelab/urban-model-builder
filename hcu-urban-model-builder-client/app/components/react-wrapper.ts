2d652e58b9a7ad4ce6365ef3a6464043e0e0de55
Nico Lazarus <n.lazarus@triggerco.de>
2024-09-03 17:25:57 +0200

    ✨ added EmberReactConnectorService

--- hcu-urban-model-builder-client/app/components/react-wrapper.ts
+++ hcu-urban-model-builder-client/app/components/react-wrapper.ts
@@ -1,13 +1,10 @@
 import { initReact } from 'hcu-urban-model-builder-react-canvas/index.tsx';
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 import Component from '@glimmer/component';
-import type Node from 'hcu-urban-model-builder-client/models/node';
 import { service } from '@ember/service';
-import type Store from '@ember-data/store';
-import type Edge from 'hcu-urban-model-builder-client/models/edge';
 import type ModelModel from 'hcu-urban-model-builder-client/models/model';
 import fade from 'ember-animated/transitions/fade';
+import type EmberReactConnectorService from 'hcu-urban-model-builder-client/services/ember-react-connector';
 
 export interface ReactWrapperSignature {
   // The arguments accepted by the component
@@ -25,116 +22,39 @@ export interface ReactWrapperSignature {
 export default class ReactWrapperComponent extends Component<ReactWrapperSignature> {
   readonly transition = fade;
 
-  @service declare store: Store;
-
-  @tracked selected: (Node | Edge)[] = [];
-  @tracked sidebarElement: HTMLElement | null = null;
+  @service declare emberReactConnector: EmberReactConnectorService;
 
   get canInsert() {
     return !!this.args.model;
   }
 
-  get nodeActions() {
-    return {
-      save: this.save,
-      create: this.create,
-      delete: this.delete,
-      select: this.select,
-      unselect: this.unselect,
-      onSidebarInserted: this.onSidebarInserted,
-    };
-  }
-
   get hasSelected() {
-    return this.selected.length > 0;
+    return this.emberReactConnector.selected.length > 0;
   }
 
   get selectedSingleItem() {
-    if (this.selected.length === 1) {
-      return this.selected[0];
+    if (this.emberReactConnector.selected.length === 1) {
+      return this.emberReactConnector.selected[0];
     }
     return null;
   }
 
   get selectedMultipleItems() {
-    if (this.selected.length > 1) {
-      return this.selected;
+    if (this.emberReactConnector.selected.length > 1) {
+      return this.emberReactConnector.selected;
     }
     return null;
   }
 
   @action
   async didInsert(element: HTMLElement) {
+    this.emberReactConnector.currentModel = this.args.model;
+
     initReact(
       element,
       await this.args.model.nodes,
       await this.args.model.edges,
-      this.nodeActions,
+      this.emberReactConnector,
     );
-  }
-
-  @action
-  async save(type: 'node' | 'edge', id: string, rawData: any) {
-    const record = this.store.peekRecord<Node | Edge>(type, id);
-    if (!record) {
-      throw new Error(`Node with id ${id} not found`);
-    }
-
-    const data = { ...rawData };
-    for (const key of ['source', 'target']) {
-      console.log('createNode', key, rawData[key]);
-
-      if (key in rawData) {
-        data[key] = this.store.peekRecord<Node>('node', rawData[key]);
-      }
-    }
-    Object.assign(record, data);
-    await record.save();
-  }
-
-  @action
-  async create(type: 'node' | 'edge', rawData: any) {
-    const data = { ...rawData };
-    for (const key of ['source', 'target']) {
-      console.log('createNode', key, rawData[key]);
-
-      if (key in rawData) {
-        data[key] = this.store.peekRecord<Node>('node', rawData[key]);
-      }
-    }
-    data.model = this.args.model;
-
-    return this.store.createRecord<Node | Edge>(type, data).save();
-  }
-
-  @action
-  async delete(type: 'node' | 'edge', id: string) {
-    this.selected = this.selected.filter((r) => r.id !== id);
-    const record = this.store.peekRecord<Node | Edge>(type, id);
-    if (!record) {
-      throw new Error(`Node with id ${id} not found`);
-    }
-    record.deleteRecord();
-    await record.save();
-  }
-
-  @action
-  select(type: 'node' | 'edge', id: string) {
-    console.log('select', type, id);
-    const record = this.store.peekRecord<Node | Edge>(type, id);
-    if (!record) {
-      throw new Error(`Node with id ${id} not found`);
-    }
-    this.selected = [...this.selected, record];
-  }
-
-  @action
-  unselect(type: 'node' | 'edge', id: string) {
-    console.log('unselect', type, id);
-    this.selected = this.selected.filter((r) => r.id !== id);
-  }
-
-  @action onSidebarInserted(element: HTMLElement) {
-    this.sidebarElement = element;
   }
 }
---
+++ hcu-urban-model-builder-client/app/services/ember-react-connector.ts
@@ -0,0 +1,91 @@
+import type Store from '@ember-data/store';
+import { action } from '@ember/object';
+import Service, { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import type Edge from 'hcu-urban-model-builder-client/models/edge';
+import type ModelModel from 'hcu-urban-model-builder-client/models/model';
+import type Node from 'hcu-urban-model-builder-client/models/node';
+
+export default class EmberReactConnectorService extends Service {
+  @service declare store: Store;
+
+  @tracked selected: (Node | Edge)[] = [];
+  @tracked currentModel: ModelModel | null = null;
+  @tracked sidebarElement: HTMLElement | null = null;
+
+  @action
+  async save(type: 'node' | 'edge', id: string, rawData: any) {
+    const record = this.store.peekRecord<Node | Edge>(type, id);
+    if (!record) {
+      throw new Error(`Node with id ${id} not found`);
+    }
+
+    const data = { ...rawData };
+    for (const key of ['source', 'target']) {
+      console.log('createNode', key, rawData[key]);
+
+      if (key in rawData) {
+        data[key] = this.store.peekRecord<Node>('node', rawData[key]);
+      }
+    }
+    Object.assign(record, data);
+    await record.save();
+  }
+
+  @action
+  async create(type: 'node' | 'edge', rawData: any) {
+    const data = { ...rawData };
+    for (const key of ['source', 'target']) {
+      console.log('createNode', key, rawData[key]);
+      if (key in rawData) {
+        data[key] = this.store.peekRecord<Node>('node', rawData[key]);
+      }
+    }
+
+    data.model = this.currentModel;
+
+    return this.store.createRecord<Node | Edge>(type, data).save();
+  }
+
+  @action
+  async delete(type: 'node' | 'edge', id: string) {
+    this.selected = this.selected.filter((r) => r.id !== id);
+    const record = this.store.peekRecord<Node | Edge>(type, id);
+    if (!record) {
+      throw new Error(`Node with id ${id} not found`);
+    }
+    record.deleteRecord();
+    await record.save();
+  }
+
+  @action
+  select(type: 'node' | 'edge', id: string) {
+    console.log('select', type, id);
+    const record = this.store.peekRecord<Node | Edge>(type, id);
+    if (!record) {
+      throw new Error(`Node with id ${id} not found`);
+    }
+    this.selected = [...this.selected, record];
+  }
+
+  @action
+  unselect(type: 'node' | 'edge', id: string) {
+    console.log('unselect', type, id);
+    this.selected = this.selected.filter((r) => r.id !== id);
+  }
+
+  @action
+  onSidebarInserted(element: HTMLElement) {
+    this.sidebarElement = element;
+  }
+}
+
+// Don't remove this declaration: this is what enables TypeScript to resolve
+// this service using `Owner.lookup('service:ember-react-connector')`, as well
+// as to check when you pass the service name as an argument to the decorator,
+// like `@service('ember-react-connector') declare altName: EmberReactConnectorService;`.
+declare module '@ember/service' {
+  interface Registry {
+    'ember-react-connector': EmberReactConnectorService;
+  }
+}
