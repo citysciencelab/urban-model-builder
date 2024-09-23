d9a51b77f3423a887618c2246db73f9c4738c5c4
Nico Lazarus <n.lazarus@triggerco.de>
2024-09-23 11:25:55 +0200

    🔨refactored simulation adapter

--- hcu-urban-model-builder-backend/src/simulation-adapter/simulation-adapter.ts
+++ hcu-urban-model-builder-backend/src/simulation-adapter/simulation-adapter.ts
@@ -2,23 +2,43 @@ import { Model } from 'simulation'
 import { Application } from '../declarations.js'
 import { Nodes, NodeType } from '../services/nodes/nodes.shared.js'
 import { Agent, Container, Flow, Population, Primitive, State, Stock, Transition } from 'simulation/blocks'
-import { EdgeType } from '../services/edges/edges.shared.js'
+import { Edges, EdgeType } from '../services/edges/edges.shared.js'
 import { primitiveFactory } from './primitive-factory.js'
+import { logger } from '../logger.js'
+import { Results } from 'simulation/Results'
 
 type PopulationNodeResult = {
   location: [number, number]
-  state: {
-    id: string
-  }[]
+  state: number[]
 }[][]
 
 export class SimulationAdapter {
+  private nodeIdPrimitiveMap = new Map<number, Primitive>()
+  private primitiveIdNodeIdMap: Map<string, number> = new Map()
+  private primitiveIdTypeMap = new Map<string, NodeType>()
+
   constructor(
     private app: Application,
     private modelId: number
   ) {}
 
-  async simulate() {
+  public async simulate() {
+    const model = await this.createSimulationModel()
+
+    await this.createModelPrimitives(model)
+
+    await this.assignPrimitiveParents()
+
+    await this.assignAgentToPopulation()
+
+    await this.createModelRelationsByEdges(model)
+
+    const simulationResult = model.simulate()
+
+    return this.serializeSimulationResult(simulationResult)
+  }
+
+  private async createSimulationModel() {
     const modelInDB = await this.app.service('models').get(this.modelId)
 
     const model = new Model({
@@ -31,22 +51,25 @@ export class SimulationAdapter {
 
     model.globals = modelInDB.globals || ''
 
+    return model
+  }
+
+  private async createModelPrimitives(model: Model) {
     const nodes = await this.app.service('nodes').find({
       query: {
         modelId: this.modelId
       }
     })
 
-    const nodeIdPrimitiveMap = new Map<number, Primitive>()
-    const primitiveIdTypeMap = new Map<string, NodeType>()
-
     for (const node of nodes.data) {
-      const type = node.type
       const simulationPrimitive = await primitiveFactory(model, node)
-      nodeIdPrimitiveMap.set(node.id, simulationPrimitive)
-      primitiveIdTypeMap.set(simulationPrimitive.id, node.type)
+      this.nodeIdPrimitiveMap.set(node.id, simulationPrimitive)
+      this.primitiveIdNodeIdMap.set(simulationPrimitive.id, node.id)
+      this.primitiveIdTypeMap.set(simulationPrimitive.id, node.type)
     }
+  }
 
+  private async assignPrimitiveParents() {
     const nodesWithParent = await this.app.service('nodes').find({
       query: {
         modelId: this.modelId,
@@ -57,8 +80,8 @@ export class SimulationAdapter {
     })
 
     for (const node of nodesWithParent.data) {
-      const parentPrimitive = nodeIdPrimitiveMap.get(node.parentId!) as Container
-      const childPrimitive = nodeIdPrimitiveMap.get(node.id) as Primitive
+      const parentPrimitive = this.nodeIdPrimitiveMap.get(node.parentId!) as Container
+      const childPrimitive = this.nodeIdPrimitiveMap.get(node.id) as Primitive
       if (!parentPrimitive) {
         throw new Error('Parent primitive not found')
       }
@@ -67,7 +90,9 @@ export class SimulationAdapter {
       }
       childPrimitive.parent = parentPrimitive
     }
+  }
 
+  private async assignAgentToPopulation() {
     const allPopulations = await this.app.service('nodes').find({
       query: {
         modelId: this.modelId,
@@ -76,14 +101,16 @@ export class SimulationAdapter {
     })
 
     for (const population of allPopulations.data) {
-      const populationPrimitive = nodeIdPrimitiveMap.get(population.id) as Population
+      const populationPrimitive = this.nodeIdPrimitiveMap.get(population.id) as Population
 
       if (population.data?.agentBaseId) {
-        const relatedAgent = nodeIdPrimitiveMap.get(population.data.agentBaseId) as Agent
+        const relatedAgent = this.nodeIdPrimitiveMap.get(population.data.agentBaseId) as Agent
         populationPrimitive.agentBase = relatedAgent
       }
     }
+  }
 
+  private async createModelRelationsByEdges(model: Model) {
     const edges = await this.app.service('edges').find({
       query: {
         modelId: this.modelId
@@ -92,63 +119,34 @@ export class SimulationAdapter {
 
     for (const edge of edges.data) {
       if (edge.type === EdgeType.Link) {
-        const sourcePrimitive = nodeIdPrimitiveMap.get(edge.sourceId)
-        const targetPrimitive = nodeIdPrimitiveMap.get(edge.targetId)
+        const sourcePrimitive = this.nodeIdPrimitiveMap.get(edge.sourceId)
+        const targetPrimitive = this.nodeIdPrimitiveMap.get(edge.targetId)
         if (sourcePrimitive && targetPrimitive) {
           model.Link(sourcePrimitive, targetPrimitive)
         }
       } else if (edge.type === EdgeType.Flow) {
-        if (edge.sourceHandle.startsWith('flow')) {
-          const targetPrimitive = nodeIdPrimitiveMap.get(edge.targetId) as Stock
-          const flow = nodeIdPrimitiveMap.get(edge.sourceId) as Flow
-
-          flow.end = targetPrimitive
-        } else if (edge.targetHandle.startsWith('flow')) {
-          const sourcePrimitive = nodeIdPrimitiveMap.get(edge.sourceId) as Stock
-          const flow = nodeIdPrimitiveMap.get(edge.targetId) as Flow
-
-          flow.start = sourcePrimitive
-        } else {
-          throw new Error('Invalid flow edge. Must have a flow handle')
-        }
+        this.setPrimitiveStartEndByEdge(edge, 'flow')
       } else if (edge.type === EdgeType.Transition) {
-        if (edge.sourceHandle.startsWith('transition')) {
-          const sourcePrimitive = nodeIdPrimitiveMap.get(edge.targetId) as State
-          const transition = nodeIdPrimitiveMap.get(edge.sourceId) as Transition
-
-          transition.end = sourcePrimitive
-        } else if (edge.targetHandle.startsWith('transition')) {
-          const targetPrimitive = nodeIdPrimitiveMap.get(edge.sourceId) as State
-          const transition = nodeIdPrimitiveMap.get(edge.targetId) as Transition
-
-          transition.start = targetPrimitive
-        } else {
-          throw new Error('Invalid transition edge. Must have a transition handle')
-        }
+        this.setPrimitiveStartEndByEdge(edge, 'transition')
       }
     }
+  }
 
-    const simulationResult = model.simulate()
-
-    const primitiveIdMap: Record<string, number> = {}
-    for (const [nodeId, primitive] of nodeIdPrimitiveMap.entries()) {
-      primitiveIdMap[primitive.id] = nodeId
-    }
-
+  private serializeSimulationResult(simulationResult: Results) {
     const resultData = {
       nodes: {} as Record<number, { series: number[] | PopulationNodeResult }>,
       times: simulationResult.times()
     }
-    for (const [nodeId, primitive] of nodeIdPrimitiveMap) {
+    for (const [nodeId, primitive] of this.nodeIdPrimitiveMap) {
       if (primitive.id in simulationResult._data.children!) {
         const primitiveResult = simulationResult.series(primitive)
 
         let series: number[] | PopulationNodeResult = []
-        if (NodeType.Population === primitiveIdTypeMap.get(primitive.id)) {
+        if (NodeType.Population === this.primitiveIdTypeMap.get(primitive.id)) {
           series = primitiveResult.map((value: any) =>
             value.current.map((item: { state: { id: string }[]; location: { items: [number, number] } }) => ({
               location: item.location.items,
-              state: item.state.map((s: { id: string }) => primitiveIdMap[s.id])
+              state: item.state.map((s: { id: string }) => this.primitiveIdNodeIdMap.get(s.id))
             }))
           )
         } else {
@@ -159,10 +157,28 @@ export class SimulationAdapter {
           series
         }
       } else {
-        console.debug('No result for primitive with id:', primitive.id)
+        logger.debug('No result for primitive with id:', primitive.id)
       }
     }
 
     return resultData
+  }
+
+  private setPrimitiveStartEndByEdge<T extends 'flow' | 'transition'>(edge: Edges, handlePrefix: T) {
+    if (edge.sourceHandle.startsWith(handlePrefix)) {
+      const flowOrTransition = this.nodeIdPrimitiveMap.get(edge.sourceId) as T extends 'flow'
+        ? Flow
+        : Transition
+
+      flowOrTransition.end = this.nodeIdPrimitiveMap.get(edge.targetId) as T extends 'flow' ? Stock : State
+    } else if (edge.targetHandle.startsWith(handlePrefix)) {
+      const flowOrTransition = this.nodeIdPrimitiveMap.get(edge.targetId) as T extends 'flow'
+        ? Flow
+        : Transition
+
+      flowOrTransition.start = this.nodeIdPrimitiveMap.get(edge.sourceId) as T extends 'flow' ? Stock : State
+    } else {
+      throw new Error(`Invalid ${handlePrefix} edge. Must have a ${handlePrefix} handle. Edge id: ${edge.id}`)
+    }
   }
 }
