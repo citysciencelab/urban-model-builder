c3dc3181b509f50c517d20cfc5f7d707fb1e1934
Nico Lazarus <n.lazarus@triggerco.de>
2024-09-26 13:41:04 +0200

    🐞 state can be null if not states assigned to agent

--- hcu-urban-model-builder-backend/src/shared/simulation-adapter/simulation-adapter.ts
+++ hcu-urban-model-builder-backend/src/shared/simulation-adapter/simulation-adapter.ts
@@ -151,10 +151,14 @@ export class SimulationAdapter<T extends ClientApplication | Application> {
         if (NodeType.Population === this.primitiveIdTypeMap.get(primitive.id)) {
           series = primitiveResult.map((value: any) =>
             value.current.map(
-              (item: { instanceId: string; state: { id: string }[]; location: { items: [number, number] } }) => ({
+              (item: {
+                instanceId: string
+                state: { id: string }[]
+                location: { items: [number, number] }
+              }) => ({
                 id: item.instanceId,
                 location: item.location.items,
-                state: item.state.map((s: { id: string }) => this.primitiveIdNodeIdMap.get(s.id))
+                state: item.state?.map((s: { id: string }) => this.primitiveIdNodeIdMap.get(s.id)) || []
               })
             )
           )
