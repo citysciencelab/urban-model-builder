ef82cd09edb4040aa1bdcb6bf204cdff83736451
Markus Ritter <m.ritter@triggerco.de>
2024-11-08 16:53:11 +0100

    ✨ adjusted deployment sscript

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
+su modelbuilder
 git pull
-
 cd ../hcu-urban-model-builder-client && npm run build
-
+exit
 docker compose -f docker-compose-staging.yml build --no-cache
 docker compose -f docker-compose-staging.yml down
 docker compose -f docker-compose-staging.yml up -d 
