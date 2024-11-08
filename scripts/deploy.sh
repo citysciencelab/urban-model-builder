a3fd8a1aab7f6dc35fccd88d285064aa8ab0f54f
Markus Ritter <m.ritter@triggerco.de>
2024-11-08 17:15:50 +0100

    ✨ improved deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-su modelbuilder
+su - modelbuilder -c "
+cd /var/www/vhosts/comodeling.city/modelbuilder/urban-model-builder
 git pull
-cd ../hcu-urban-model-builder-client && npm run build
-exit
+cd hcu-urban-model-builder-client && npm i && npm run build
+"
+
 docker compose -f docker-compose-staging.yml build --no-cache
 docker compose -f docker-compose-staging.yml down
 docker compose -f docker-compose-staging.yml up -d 
