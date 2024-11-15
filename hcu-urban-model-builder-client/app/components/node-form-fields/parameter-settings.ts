59ae494a919cc3345a0b61c9330b962403af14d7
Tobias Braner <t.braner@triggerco.de>
2024-11-15 15:43:46 +0100

    🎨 improved default parameter handling and handling when value is present

--- hcu-urban-model-builder-client/app/components/node-form-fields/parameter-settings.ts
+++ hcu-urban-model-builder-client/app/components/node-form-fields/parameter-settings.ts
@@ -1,6 +1,7 @@
 import Component from '@glimmer/component';
 import type Node from 'hcu-urban-model-builder-client/models/node';
 import { action } from '@ember/object';
+import { next } from '@ember/runloop';
 
 export interface NodeFormFieldsParameterSettingsSignature {
   // The arguments accepted by the component
@@ -17,11 +18,42 @@ export interface NodeFormFieldsParameterSettingsSignature {
 }
 
 export default class NodeFormFieldsParameterSettingsComponent extends Component<NodeFormFieldsParameterSettingsSignature> {
-  @action toggleParameter() {
-    this.args.changeset.isParameter = !this.args.changeset.isParameter;
+  @action toggleParameter(event: Event) {
+    const newToggleValue = !this.args.changeset.isParameter;
+
+    let yesNo = true;
+    if (newToggleValue == true && this.args.changeset.data.value) {
+      yesNo = confirm(
+        'When you use this node as parameter the current value will not be used, proceed?',
+      );
+    }
+
+    if (!yesNo) {
+      // hack to reset the toggle parameter value
+      this.args.changeset.isParameter = true;
+      next(() => {
+        this.args.changeset.isParameter = false;
+      });
+      return false;
+    }
+
+    this.args.changeset.isParameter = newToggleValue;
+
     if (this.args.type == 'boolean' && this.args.changeset.isParameter) {
+      // set values to 0/1 respectively (true/false)
       this.args.changeset.parameterMin = 0;
       this.args.changeset.parameterMax = 1;
+    } else if (this.args.type != 'boolean' && this.args.changeset.isParameter) {
+      // potentialy initialize the min and max values
+      if (!this.args.changeset.parameterMin) {
+        this.args.changeset.parameterMin = 0;
+      }
+      if (!this.args.changeset.parameterMax) {
+        this.args.changeset.parameterMax = 1;
+      }
+      if (!this.args.changeset.parameterStep) {
+        this.args.changeset.parameterStep = 0.1;
+      }
     }
   }
 }
