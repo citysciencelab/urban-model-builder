accdd85a4905dd3426b603081d5aeba786eb004b
Nico Lazarus <n.lazarus@triggerco.de>
2025-01-16 15:54:58 +0100

    ✨added logs logging hook

--- hcu-urban-model-builder-backend/src/hooks/log-error.ts
+++ hcu-urban-model-builder-backend/src/hooks/log-error.ts
@@ -1,11 +1,15 @@
 // For more information about this file see https://dove.feathersjs.com/guides/cli/log-error.html
+import { log } from 'console'
 import type { HookContext, NextFunction } from '../declarations.js'
 import { logger } from '../logger.js'
 
 export const logError = async (context: HookContext, next: NextFunction) => {
   try {
+    logger.debug(`BEFORE ${context.path}.${context.method}()`)
     await next()
+    logger.debug(`AFTER ${context.path}.${context.method}()`)
   } catch (error: any) {
+    logger.error(`ERROR ${context.path}.${context.method}()`)
     logger.error(error.stack)
 
     // Log validation errors
