24ba83229f2a1679f04bc85a232b4881368a7c32
Dennis Triggercode <dennis.krieger.triggerco.de>
2025-02-19 10:07:09 +0100

    🐞 moved args into primitives modal

--- hcu-urban-model-builder-client/app/components/node-toolbar.ts
+++ hcu-urban-model-builder-client/app/components/node-toolbar.ts
@@ -1,13 +1,9 @@
 import type Store from '@ember-data/store';
-import { action } from '@ember/object';
-import { service } from '@ember/service';
 import Component from '@glimmer/component';
-import { NodeType } from 'hcu-urban-model-builder-backend';
 import type ModelModel from 'hcu-urban-model-builder-client/models/model';
-import type EmberReactConnectorService from 'hcu-urban-model-builder-client/services/ember-react-connector';
+import { service } from '@ember/service';
+
 import type StoreEventEmitterService from 'hcu-urban-model-builder-client/services/store-event-emitter';
-import { decamelize, dasherize } from '@ember/string';
-import { tracked } from '@glimmer/tracking';
 import type ModelDialogsService from 'hcu-urban-model-builder-client/services/model-dialogs';
 
 export interface NodeToolbarSignature {
@@ -23,87 +19,7 @@ export interface NodeToolbarSignature {
   Element: null;
 }
 
-const NodeIconMap: Record<string, string> = {
-  [NodeType.Stock]: 'inventory',
-  [NodeType.Variable]: 'category',
-  [NodeType.Flow]: 'flow-icon',
-  [NodeType.Converter]: 'autorenew',
-  [NodeType.State]: 'toggle_off',
-  [NodeType.Transition]: 'transition_push',
-  [NodeType.Action]: 'play_pause',
-  [NodeType.Population]: 'groups',
-  [NodeType.Agent]: 'person',
-  [NodeType.Folder]: 'folder',
-  [NodeType.Ghost]: 'ghost',
-  [NodeType.OgcApiFeatures]: 'storage',
-};
-
-type NodeTypeConfig = {
-  label: string;
-  className: string;
-  value: NodeType;
-  icon: keyof typeof NodeIconMap | 'question-circle';
-};
-
 export default class NodeToolbarComponent extends Component<NodeToolbarSignature> {
   @service declare store: Store;
-  @service declare modelDialogs: ModelDialogsService;
   @service declare storeEventEmitter: StoreEventEmitterService;
-  @service declare emberReactConnector: EmberReactConnectorService;
-  @tracked isPinned = false;
-
-  readonly nodeGroups = [
-    {
-      name: 'common',
-      nodeTypes: [
-        NodeType.Variable,
-        NodeType.Folder,
-        NodeType.Converter,
-        NodeType.OgcApiFeatures,
-      ],
-    },
-    {
-      name: 'system-dynamics',
-      nodeTypes: [NodeType.Stock, NodeType.Flow],
-    },
-    {
-      name: 'agent-based-modelling',
-      nodeTypes: [
-        NodeType.State,
-        NodeType.Transition,
-        NodeType.Action,
-        NodeType.Population,
-        NodeType.Agent,
-      ],
-    },
-  ];
-
-  @action getNodeTypeConfig(nodeType: NodeType): NodeTypeConfig {
-    const typeStr = NodeType[nodeType];
-
-    return {
-      label: decamelize(typeStr),
-      className: dasherize(typeStr),
-      value: nodeType,
-      icon: NodeIconMap[nodeType] || 'question-circle',
-    };
-  }
-
-  @action onDragStart(config: NodeTypeConfig) {
-    this.emberReactConnector.draggedNodeConfig = config;
-  }
-
-  @action
-  async onDragEnd(event: DragEvent) {
-    event.preventDefault();
-    this.emberReactConnector.draggedNodeConfig = null;
-  }
-
-  @action togglePin() {
-    this.isPinned = !this.isPinned;
-  }
-
-  @action onClose() {
-    return !this.isPinned;
-  }
 }
--- hcu-urban-model-builder-client/app/components/node-toolbar/primitives-modal.ts
+++ hcu-urban-model-builder-client/app/components/node-toolbar/primitives-modal.ts
@@ -1,4 +1,11 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+
+import { NodeType } from 'hcu-urban-model-builder-backend';
+import { decamelize, dasherize } from '@ember/string';
+import type EmberReactConnectorService from 'hcu-urban-model-builder-client/services/ember-react-connector';
 
 export interface NodeToolbarPrimitivesModalSignature {
   // The arguments accepted by the component
@@ -11,4 +18,84 @@ export interface NodeToolbarPrimitivesModalSignature {
   Element: null;
 }
 
-export default class NodeToolbarPrimitivesModalComponent extends Component<NodeToolbarPrimitivesModalSignature> { }
+const NodeIconMap: Record<string, string> = {
+  [NodeType.Stock]: 'inventory',
+  [NodeType.Variable]: 'category',
+  [NodeType.Flow]: 'flow-icon',
+  [NodeType.Converter]: 'autorenew',
+  [NodeType.State]: 'toggle_off',
+  [NodeType.Transition]: 'transition_push',
+  [NodeType.Action]: 'play_pause',
+  [NodeType.Population]: 'groups',
+  [NodeType.Agent]: 'person',
+  [NodeType.Folder]: 'folder',
+  [NodeType.Ghost]: 'ghost',
+  [NodeType.OgcApiFeatures]: 'storage',
+};
+
+type NodeTypeConfig = {
+  label: string;
+  className: string;
+  value: NodeType;
+  icon: keyof typeof NodeIconMap | 'question-circle';
+};
+
+export default class NodeToolbarPrimitivesModalComponent extends Component<NodeToolbarPrimitivesModalSignature> {
+  @tracked isPinned = false;
+  @service declare emberReactConnector: EmberReactConnectorService;
+
+  readonly nodeGroups = [
+    {
+      name: 'common',
+      nodeTypes: [
+        NodeType.Variable,
+        NodeType.Folder,
+        NodeType.Converter,
+        NodeType.OgcApiFeatures,
+      ],
+    },
+    {
+      name: 'system-dynamics',
+      nodeTypes: [NodeType.Stock, NodeType.Flow],
+    },
+    {
+      name: 'agent-based-modelling',
+      nodeTypes: [
+        NodeType.State,
+        NodeType.Transition,
+        NodeType.Action,
+        NodeType.Population,
+        NodeType.Agent,
+      ],
+    },
+  ];
+
+  @action getNodeTypeConfig(nodeType: NodeType): NodeTypeConfig {
+    const typeStr = NodeType[nodeType];
+
+    return {
+      label: decamelize(typeStr),
+      className: dasherize(typeStr),
+      value: nodeType,
+      icon: NodeIconMap[nodeType] || 'question-circle',
+    };
+  }
+
+  @action togglePin() {
+    this.isPinned = !this.isPinned;
+  }
+
+  @action onClose() {
+    return !this.isPinned;
+  }
+
+  @action onDragStart(config: NodeTypeConfig) {
+    this.emberReactConnector.draggedNodeConfig = config;
+  }
+
+  @action
+  async onDragEnd(event: DragEvent) {
+    event.preventDefault();
+    this.emberReactConnector.draggedNodeConfig = null;
+  }
+}
