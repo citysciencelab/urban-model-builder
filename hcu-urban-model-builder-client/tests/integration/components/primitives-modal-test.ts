9b8899b412217e3412dbe8b4e11258ac38eeb829
Dennis Triggercode <dennis.krieger.triggerco.de>
2025-02-19 09:25:17 +0100

    ✨ primitives modal

---
+++ hcu-urban-model-builder-client/tests/integration/components/primitives-modal-test.ts
@@ -0,0 +1,26 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'hcu-urban-model-builder-client/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | primitives-modal', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<PrimitivesModal />`);
+
+    assert.dom().hasText('');
+
+    // Template block usage:
+    await render(hbs`
+      <PrimitivesModal>
+        template block text
+      </PrimitivesModal>
+    `);
+
+    assert.dom().hasText('template block text');
+  });
+});
