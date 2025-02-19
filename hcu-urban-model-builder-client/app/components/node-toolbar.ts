85e67e5be9cbe491389085085f285458e8ae0724
Dennis Triggercode <dennis.krieger.triggerco.de>
2025-02-19 10:09:44 +0100

    🐞 cleaned node-toolbar.ts

--- hcu-urban-model-builder-client/app/components/node-toolbar.ts
+++ hcu-urban-model-builder-client/app/components/node-toolbar.ts
@@ -1,10 +1,5 @@
-import type Store from '@ember-data/store';
 import Component from '@glimmer/component';
 import type ModelModel from 'hcu-urban-model-builder-client/models/model';
-import { service } from '@ember/service';
-
-import type StoreEventEmitterService from 'hcu-urban-model-builder-client/services/store-event-emitter';
-import type ModelDialogsService from 'hcu-urban-model-builder-client/services/model-dialogs';
 
 export interface NodeToolbarSignature {
   // The arguments accepted by the component
@@ -19,7 +14,4 @@ export interface NodeToolbarSignature {
   Element: null;
 }
 
-export default class NodeToolbarComponent extends Component<NodeToolbarSignature> {
-  @service declare store: Store;
-  @service declare storeEventEmitter: StoreEventEmitterService;
-}
+export default class NodeToolbarComponent extends Component<NodeToolbarSignature> { }
