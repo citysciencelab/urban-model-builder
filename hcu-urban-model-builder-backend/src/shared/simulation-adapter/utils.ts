bcc2f3fd38fdf84785bc6f062887a0e1c0c96682
Nico Lazarus <n.lazarus@triggerco.de>
2025-02-19 07:52:09 +0100

    🐞fixed ogc feature api data transformation

--- hcu-urban-model-builder-backend/src/shared/simulation-adapter/utils.ts
+++ hcu-urban-model-builder-backend/src/shared/simulation-adapter/utils.ts
@@ -8,25 +8,25 @@ export function transformFeatures(features: Feature[], key?: string, value?: str
   return toSimulationVectorString(data, pretty)
 }
 
-export function transformData(data: Feature[], key?: string, value?: string[]) {
-  const valueTransformMethod = getValueTransformMethod(value)
+export function transformData(data: Feature[], keyProperty?: string, valueProperties?: string[]) {
+  const valueTransformFunc = getValueTransformFunc(valueProperties)
 
-  if (key && value) {
+  if (keyProperty && valueProperties) {
     return data.reduce(
       (acc, item) => {
-        acc[item.properties![key]] = valueTransformMethod(item)
+        acc[item.properties![keyProperty]] = valueTransformFunc(item)
         return acc
       },
       {} as Record<string, any>
     )
-  } else if (value && value.length > 1) {
-    return data.map((item) => valueTransformMethod(item))
+  } else if (valueProperties && valueProperties.length > 0) {
+    return data.map((item) => valueTransformFunc(item))
   }
 
   return data
 }
 
-function getValueTransformMethod(valueProperties?: string[]) {
+function getValueTransformFunc(valueProperties?: string[]) {
   if (valueProperties && valueProperties.length === 1) {
     return (item: Feature) => getValueByKey(item, valueProperties[0])
   } else if (valueProperties && valueProperties.length > 1) {
