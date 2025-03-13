7155c6cd549d2c05697840ec3d301ce06702b638
Tobias Braner <t.braner@triggerco.de>
2025-03-13 10:37:17 +0100

    🐞 fix preloading but with scenarios and values

--- hcu-urban-model-builder-client/app/routes/models/versions/show.ts
+++ hcu-urban-model-builder-client/app/routes/models/versions/show.ts
@@ -1,11 +1,19 @@
 import type Store from '@ember-data/store';
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
-
+import type ModelsVersion from 'hcu-urban-model-builder-client/models/models-version';
 export default class ModelsShowRoute extends Route {
   @service declare store: Store;
 
   model(params: { version_id: string }) {
     return this.store.findRecord('models-version', params.version_id);
+  }
+
+  async afterModel(model: ModelsVersion) {
+    // preload scenarios and scenariosValues for simulation
+    const scenarios = await model.scenarios;
+    for (const scenario of scenarios) {
+      await scenario.scenariosValues;
+    }
   }
 }
