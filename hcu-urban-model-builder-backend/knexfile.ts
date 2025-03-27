64dc69c5f5f51e6755c7dec2392346b9a65a9330
Nico Lazarus <n.lazarus@triggerco.de>
2025-03-27 10:21:25 +0100

    🐳✨ added migration container and migrate script

--- hcu-urban-model-builder-backend/knexfile.ts
+++ hcu-urban-model-builder-backend/knexfile.ts
@@ -1,8 +1,8 @@
 // For more information about this file see https://dove.feathersjs.com/guides/cli/databases.html
-import { app } from './src/app.js'
+import nodeConfig from 'config'
 
 // Load our database connection info from the app configuration
-const config = app.get('postgresql')
+const config = nodeConfig.get('postgresql') as Record<string, any>
 
 export const onUpdateTrigger = (table: string) => `
   CREATE TRIGGER ${table}_updated_at
@@ -11,4 +11,4 @@ export const onUpdateTrigger = (table: string) => `
   EXECUTE PROCEDURE on_update_timestamp();
 `
 
-export default config
+export default { ...config }
---
+++ scripts/migrate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+su - modelbuilder -c "
+cd /var/www/vhosts/comodeling.city/modelbuilder/urban-model-builder
+git checkout main && git restore . && git pull
+"
+
+docker compose -f docker-compose-staging-migrate.yml build --no-cache
+docker compose -f docker-compose-staging-migrate.yml down
+docker compose -f docker-compose-staging-migrate.yml up -d 
